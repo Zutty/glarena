060a32a2af04d4da2e45475be4eb7738fc1613f4
George Weller <george.weller@gmail.com>
2013-07-21 15:45:15 +0100

    Rename billboard/ directory to bullet/, reflecting whats actually in there.

--- src/main/java/uk/co/zutty/glarena/BulletEmitter.java
+++ src/main/java/uk/co/zutty/glarena/BulletEmitter.java
@@ -1,15 +1,6 @@
 package uk.co.zutty.glarena;
 
-import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL30;
-import org.lwjgl.util.vector.Matrix4f;
 import uk.co.zutty.glarena.vertex.VertexFormat;
-
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
-import static org.lwjgl.opengl.GL13.glActiveTexture;
-import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL20.*;
 
 /**
  * A collection of billboarded particles.
@@ -22,17 +13,17 @@ public class BulletEmitter extends Emitter {
         shader = new ShaderProgram();
 
         Shader vertexShader = new Shader(Shader.Type.VERTEX);
-        vertexShader.loadSource("/shaders/billboard/vertex.glsl");
+        vertexShader.loadSource("/shaders/bullet/vertex.glsl");
         vertexShader.compile();
         shader.attachShader(vertexShader);
 
         Shader geometryShader = new Shader(Shader.Type.GEOMETRY);
-        geometryShader.loadSource("/shaders/billboard/geometry.glsl");
+        geometryShader.loadSource("/shaders/bullet/geometry.glsl");
         geometryShader.compile();
         shader.attachShader(geometryShader);
 
         Shader fragmentShader = new Shader(Shader.Type.FRAGMENT);
-        fragmentShader.loadSource("/shaders/billboard/fragment.glsl");
+        fragmentShader.loadSource("/shaders/bullet/fragment.glsl");
         fragmentShader.compile();
         shader.attachShader(fragmentShader);
 
