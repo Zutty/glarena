4604af8dd8e54cc79a4834c6f8a2f0db5cd2041b
Zutty <george.weller@gmail.com>
2013-08-25 19:03:47 +0100

    Remove unnecessary layer of indirection between game logic and GL API. Prelude to further refactoring.

--- src/main/java/uk/co/zutty/glarena/gl/Model.java
+++ src/main/java/uk/co/zutty/glarena/gl/Model.java
@@ -3,13 +3,17 @@ package uk.co.zutty.glarena.gl;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import uk.co.zutty.glarena.Technique;
-import uk.co.zutty.glarena.vertex.VertexArray;
-import uk.co.zutty.glarena.vertex.VertexBuffer;
+import uk.co.zutty.glarena.vertex.Attribute;
 
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 
-import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL30.*;
 
 /**
  * Represents a model stored on the GPU that can be rendered.
@@ -17,9 +21,9 @@ import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
 public class Model {
 
     private Technique technique;
-    private VertexArray vertexArray;
-    private VertexBuffer vertexBuffer;
-    private VertexBuffer indexBuffer;
+    private int glVao = GL_INVALID_VALUE;
+    private int glArrayVbo = GL_INVALID_VALUE;
+    private int glIndexVbo = GL_INVALID_VALUE;
     private int numIndices;
     private int glTexture;
 
@@ -27,32 +31,36 @@ public class Model {
         this.technique = technique;
         glTexture = texture;
 
-        vertexArray = new VertexArray();
-        vertexArray.bind();
+        glVao = glGenVertexArrays();
+        glBindVertexArray(glVao);
 
-        vertexBuffer = new VertexBuffer();
-        vertexBuffer.bind();
+        glArrayVbo = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, glArrayVbo);
 
-        vertexArray.createAttributePointers(technique.getFormat());
+        int index = 0;
+        for (Attribute attribute : technique.getFormat().getAttributes()) {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(index++, attribute.getElements(), GL_FLOAT, false, technique.getFormat().getStride(), attribute.getOffset());
+        }
 
-        indexBuffer = new VertexBuffer(GL_ELEMENT_ARRAY_BUFFER);
-        indexBuffer.bind();
+        glIndexVbo = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, glIndexVbo);
 
-        vertexArray.unbind();
+        glBindVertexArray(0);
     }
 
     public void setVertexData(FloatBuffer vertexData) {
-        vertexBuffer.bind();
-        vertexBuffer.setData(vertexData);
-        vertexBuffer.unbind();
+        glBindBuffer(GL_ARRAY_BUFFER, glArrayVbo);
+        glBufferData(GL_ARRAY_BUFFER, vertexData, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
 
     public void setIndexData(ShortBuffer indexData) {
         numIndices = indexData.capacity();
 
-        indexBuffer.bind();
-        indexBuffer.setData(indexData);
-        indexBuffer.unbind();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, glIndexVbo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexData, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
 
     public Technique getTechnique() {
@@ -60,23 +68,23 @@ public class Model {
     }
 
     public void render() {
-        GL13.glActiveTexture(GL13.GL_TEXTURE0);
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, glTexture);
+        glActiveTexture(GL13.GL_TEXTURE0);
+        glBindTexture(GL11.GL_TEXTURE_2D, glTexture);
 
-        vertexArray.bind();
+        glBindVertexArray(glVao);
 
-        GL11.glDrawElements(GL11.GL_TRIANGLES, numIndices, GL11.GL_UNSIGNED_SHORT, 0);
+        glDrawElements(GL11.GL_TRIANGLES, numIndices, GL11.GL_UNSIGNED_SHORT, 0);
 
-        vertexArray.unbind();
+        glBindVertexArray(0);
     }
 
     public void destroy() {
         // Delete texture
         GL11.glDeleteTextures(glTexture);
 
-        // Delete the mesh
-        vertexArray.destroy();
-        vertexBuffer.destroy();
-        indexBuffer.destroy();
+        // Delete the arrays and buffers
+        glDeleteVertexArrays(glVao);
+        glDeleteBuffers(glArrayVbo);
+        glDeleteBuffers(glIndexVbo);
     }
 }
